43343bf9dfb111db2fee0a2a3a2b9d5a4eacfa66
Alexandra POLEAC <alexandra.poleac@gmail.com>
2025-10-12 22:36:00 +0300

    exercitiul 5 lab 1

---
+++ src/lab2/ex5/Main.java
@@ -0,0 +1,19 @@
+package lab2.ex5;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // valori intregi
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println("x = " + x);
+
+        System.out.println();
+
+        double y = in.nextDouble();
+        System.out.println("y = " + y);
+
+    }
+
+}
