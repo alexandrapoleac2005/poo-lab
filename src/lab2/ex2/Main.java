48647a4b9b26c41b44a77c6da1386641fe2693da
Alexandra POLEAC <alexandra.poleac@gmail.com>
2025-10-12 21:00:44 +0300

    exercitiul 2 lab 1

--- src/lab2/ex2/Main.java
+++ src/lab2/ex2/Main.java
@@ -1,15 +1,41 @@
-package lab2.ex2;
-
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int i;
+        float f;
+        double d;
+        boolean b;
+        char c;
+
+
+        // parsam sirul pentru fiecare tip de data
+
+        i=Integer.parseInt("123");
+        System.out.println(i);
+
+        f=Float.parseFloat("123.45f");
+        System.out.println(f);
+
+        d=Double.parseDouble("1.234567");
+        System.out.println(d);
+
+        b=Boolean.parseBoolean("true");
+        System.out.println(b);
+
+        // cateva exemple pentru clasa Character
+
+        c=Character.toUpperCase('a');
+        System.out.println(c);
+
+        c=Character.toLowerCase('A');
+        System.out.println(c);
+
+        boolean rez;
+        rez=Character.isDigit('a');
+        System.out.println(rez);
+
+        rez=Character.isLetter('c');
+        System.out.println(rez);
+
     }
 }
